f2128a34e517b4cf55412e419441c1ee1bc6c3ce
weidaolee <weidaolee@gamil.com>
2021-12-17 21:04:59 +0800

    tree (dp, traversal): two solutions

---
+++ tree/dp/RightSideView.java
@@ -0,0 +1,45 @@
+package tree.dp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import tree.TreeNode;
+
+public class RightSideView {
+    public List <Integer> rightSideView (TreeNode root) {
+        dfs(root, 0);
+        return path;
+    }
+
+    List <Integer> path = new LinkedList<>();
+    public void dfs (TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == path.size()) {
+            path.add(root.val);
+        }
+        dfs(root.right, depth + 1);
+        dfs(root.left, depth + 1);
+    }
+
+    public List <Integer> dp (TreeNode root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        List <Integer> leftPath = dp(root.left);
+        List <Integer> rightPath = dp(root.right);
+
+        if (leftPath.size() <= rightPath.size()) {
+            leftPath = null;
+        } else {
+            for (int i = 0; i < rightPath.size(); i++) {
+                leftPath.remove(0);
+            }
+            rightPath.addAll(leftPath);
+        }
+        rightPath.add(0, root.val);
+        return rightPath;
+    }
+
+}

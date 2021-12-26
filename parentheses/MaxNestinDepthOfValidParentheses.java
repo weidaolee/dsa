a9ebc8971bbe3485502c300e5fa9563b2c6f7774
weidaolee <weidaolee@gamil.com>
2021-12-27 07:57:15 +0800

    parentheses (stack): virtual stack

---
+++ parentheses/MaxNestinDepthOfValidParentheses.java
@@ -0,0 +1,22 @@
+package parentheses;
+
+public class MaxNestinDepthOfValidParentheses {
+
+
+    public int[] maxDepthAfterSplit(String seq) {
+        int [] res = new int[seq.length()];
+        char [] chs = seq.toCharArray();
+
+        int depth = 0;
+        for (int i = 0; i < chs.length; i++) {
+            if (chs[i] == '(') {
+                depth ++;
+                res[i] = depth % 2;
+            } else {
+                res[i] = depth % 2;
+                depth --;
+            }
+        }
+        return res;
+    }
+}

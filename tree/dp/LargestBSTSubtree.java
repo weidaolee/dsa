91d2a7fc1c30fefe0b1ce0b3e862316963e8cdce
weidaolee <weidaolee@gamil.com>
2022-01-02 09:04:30 +0800

    tree (dp): classic tree dp

---
+++ tree/dp/LargestBSTSubtree.java
@@ -0,0 +1,51 @@
+package tree.dp;
+
+import tree.TreeNode;
+
+public class LargestBSTSubtree {
+    public int largestBSTSubtree (TreeNode root) {
+
+        Info info = getInfo(root);
+        return info.largestSubBSTSize;
+    }
+
+    public Info getInfo (TreeNode root) {
+        if (root == null) {
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
+        }
+
+        Info left = getInfo(root.left);
+        Info right = getInfo(root.right);
+
+        int num = left.num + 1 + right.num;
+        int min = Math.min(root.val, Math.min(left.min, right.min));
+        int max = Math.max(root.val, Math.max(left.max, right.max));
+
+        boolean isBST = false;
+
+        if (left.isBST && right.isBST && left.max < root.val && root.val < right.min) {
+            isBST = true;
+        }
+
+        int largestBSTSubtree = isBST ? num : Math.max(left.largestSubBSTSize, right.largestSubBSTSize);
+
+        return new Info(isBST, num, min, max, largestBSTSubtree);
+    }
+
+    private static class Info {
+        boolean isBST;
+        int num;
+        int min;
+        int max;
+        int largestSubBSTSize;
+
+		public Info(boolean isBST, int num, int min, int max, int largestSubBSTSize) {
+            this.isBST = isBST;
+			this.num = num;
+			this.min = min;
+			this.max = max;
+            this.largestSubBSTSize = largestSubBSTSize;
+		}
+    }
+
+}

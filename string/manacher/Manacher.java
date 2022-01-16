5e0b128742a571daef5223a76f4f7063a8029508
weidaolee <weidaolee@gamil.com>
2022-01-16 18:31:28 +0800

    review (string): review manacher

--- string/manacher/Manacher.java
+++ string/manacher/Manacher.java
@@ -1,95 +1,112 @@
 package string.manacher;
 
+
 public class Manacher {
     /**
-     * 1. 先把 string 補成 char [] = [#, s0, # ,s1, #, s2, #,...#], 這樣保證能找到偶數的 palindrome.
-     * 2. 以 R 紀錄之前的最長 palindrome 的右邊界, C 紀錄其展開之中點, initial: R = C = -1
-     * 3. 以 int [] radius 紀錄以 radius [i] 展開之最常 palindrome 半徑
-     * ===================================================================
-     * for i = 0, i < arr.length:
-     *     case 1: (C .... R) i
-     *         暴力擴，更新 R, C, radius [i]
-     *     case 2-1: (C ... (l...i...r) ... R)
-     *         i < R, 且 i 能張開的最長 palindrome 被 C - R 包住
-     *         假設以 C 為對稱點, radius [i] 會等於 radius [i']
+     * Link:
+     * https://leetcode-cn.com/problems/longest-palindromic-substring
      *
-     *         =========================================================
-     *         proof: radius [i] = radius [i']
-     *         在 (R' ... C ... R) 中，r < R,
-     *         則 (R' ...(r... i'...r')... C ... (r'... i ...r) ... R)
-     *         因為以 C 為對稱點，所以 (r... i'...r') = (r'... i ...r)
-     *         又 (r'... i ...r) 已經最長，所以 (r' - 1) != (r + 1)
-     *         ========================================================= 
-     *     case 2-2: (C ......(l...i...r=R))
-     *         i < R, 且 i 能張開的最長 palindrome 被 C - R 包住
-     *         radius [i] 會等於 R - i 
-     *
-     *         =========================================================
-     *         proof: radius [i] = R - i
-     *         在 (R' ... C ... R) 中，r = R
-     *         根據定義，R 為前一 palindrome 之最右邊界，如果 r > R
-     *         則 (R' ... C ... R) 應為 (R + 1' ... C ... R + 1)
-     *         所以 r <= R, 又如果 r < R, 則屬於 case 2-1
-     *         =========================================================
-     *     case 2-3: (C ...(r'.......i..R)...r )...)
-     *         i < R, r > R
-     *         至少知道，前面 i ... R 這一段的長度是已知，從 R + 1 的位置開始找 r
      */
 
+    String s;
+    char[] arr;
+    int maxLen;
+    int begin;
+    int end;
+    int center;
 
-    public static int maxLongestPalindrome(String s) {
+    public String longestPalindrome(String s) {
         if (s == null || s.length() < 2) {
-            return s.length();
+            return s;
         }
 
-        char [] arr = getManacherArray(s);
-        int [] radius = new int [arr.length];
+        init(s);
 
-        int c = -1;   // center
-        int r = -1;   // right
-        int max = Integer.MIN_VALUE;
+        longestPalindrome();
 
-        for (int i = 0; i < arr.length; i++) {
-            int j = 0;
-            if (i > r) {
-                while(i > j && i + j < arr.length && arr[i - j] == arr[i + j]) {
-                    j ++;
+        StringBuilder ans = new StringBuilder();
+        for (int i = begin; i <= end; i++) {
+            if (arr[i] != '$') {
+                ans.append(arr[i]);
+            }
+        }
+        return ans.toString();
+    }
+
+    /**
+     * Prepare:
+     *   1. 先把 string 補成 [#, s0, #,...,#, sn, #], 以確保偶數的 palindrome 能捕捉到
+     *   2. 以 int[] radius 紀錄以 i 為中心展開之最長 palindrome 半徑
+     *   3. 以 R 紀錄之前最右 palindrome 的右邊界, C 紀錄其展開之中點
+     *
+     * Main idea:
+     *   當來到 i 位置:
+     *     case 1: (C...R) i
+     *       說明 i 來到更遠的位置:
+     *         1. 更新 C, R 更新到以 C 為中心的最遠位置
+     *         2. 更新 radius[i]
+     *
+     *     case 2 - 1: (C...(l...i...r)...R)
+     *       說明 i 落在 C...R 之間, 且 i 能撐開的最遠 palindrome 的 右邊界 r < R,
+     *       此時 radius[i] = radius[i'], 因為 (R'...C) = (C...R), 對稱點 i'
+     *       的最長半徑 radius[i'] 就是 radius[i] 的最長半徑。
+     *
+     *     case 2 - 2: (C...(l...i...r = R))
+     *       說明 i 落在 C...R 之間, 且 i 能撐開的最遠 palindrome 的 右邊界 r = R,
+     *       此時 radius[i] = R - i
+     *
+     *     case 2 - 3: (C...(l...i..R)..r)
+     *       說明 i 落在 C...R 之間, 且 i 能撐開的最遠 palindrome 的 右邊界 r = R,
+     *       至少 i ... R 這一段長度已知. 從 R + 1 位置開始找 r
+     *
+     */
+
+    private void longestPalindrome () {
+        int[] radius = new int[arr.length];
+        int R = -1;
+        int C = -1;
+
+        maxLen = Integer.MIN_VALUE;
+        begin = -1;
+        end = 0;
+
+        for (int i = 0 ; i < arr.length; i++) {
+            int r = 0;
+            if (i > R) {
+                while (i > r && i + r < arr.length && arr[i - r] == arr[i + r]) {
+                    r ++;
                     radius[i] ++;
                 }
             } else {
-                radius[i] = Math.min(radius[2 * c - i], r - i);
-                j = radius[i];
-                while(i > j && i + j < arr.length && arr[i - j] == arr[i + j]) {
-                    j ++;
+                radius[i] = Math.min (radius[2 * C - i], R - i);
+                r = radius[i];
+                while (i > r && i + r < arr.length && arr[i - r] == arr[i + r]) {
+                    r ++;
                     radius[i] ++;
                 }
             }
-            if (i + radius[i] > r) {
-                r = i + radius[i];
-                c = i;
+
+            if (i + radius[i] > R) {
+                R = i + radius[i];
+                C = i;
             }
 
-            max = Math.max(max, radius[i]);
+            if (maxLen < radius[i]) {
+                maxLen = radius[i];
+                center = i;
+            }
         }
-        return max - 1;
+
+        maxLen --;
+        begin = center - maxLen;
+        end = center + maxLen;
     }
 
-
-    public static char[] getManacherArray (String s) {
-        char [] str = s.toCharArray();
-        char [] arr = new char [str.length * 2 + 1];
-
+    private void init (String s) {
+        arr = new char[s.length() * 2 + 1];
         int index = 0;
         for (int i = 0; i < arr.length; i++) {
-            // arr[i] = i % 2 == 0 ? '#' : str[index ++];
-            arr[i] = (i & 1) == 0 ? '#' : str[index ++];
+            arr[i] = (i & 1) == 0 ? '$' : s.charAt(index ++);
         }
-
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int len = maxLongestPalindrome("");
-        System.out.println(len);
     }
 }
